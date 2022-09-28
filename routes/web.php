f144e60a90518a016cd11bc3365e8ee6d3447461
ahmdriffai <ahmmd.riffai@gmail.com>
2022-09-28 15:38:14 +0700

    menambah authentikasi dari account.feb-unsiq.ac.id

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,55 +21,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function (Request $request) {
-    
-    if ($request->session()->get('access_token') != null) {
-        return redirect()->back();
-    }
-    $state = Str::random(40);
-    $request->session()->put("state", $state);
-    $query = http_build_query([
-        'client_id' => env('CLIENT_ID'),
-        'redirect_uri' => env('REDIRECT_AUTH_URI'),
-        'response_type' => 'code',
-        'scope' => '',
-        'state' => $state,
-    ]);
+Route::controller(AuthController::class)
+    ->prefix('auth')
+    ->as('auth.')
+    ->group(function () {
+        Route::get('/login', 'login')->name('login');
+        Route::get('/callback', 'callback')->name('callback');
+    });
 
-    return redirect('https://accounts.feb-unsiq.ac.id/oauth/authorize?' . $query);
+Route::get('test', function () {
+    return 'test page';
 });
 
-Route::get('/callback', function(Request $request) {
-    $state = $request->get('state');
+Route::get('/authuser', function (Request $request) {
 
-    throw_unless(strlen($state) > 0 && $state == $request->state, InvalidArgumentException::class);
-
-    $response = Http::asForm()->post('https://accounts.feb-unsiq.ac.id/oauth/token', [
-            'grant_type' => 'authorization_code', 
-            'client_id' => env('CLIENT_ID'),
-            'client_secret' => env('CLIENT_SECRET'),
-            'redirect_uri' => env('REDIRECT_AUTH_URI'),
-            'code' => $request->code
-        ]
-    );
-    $request->session()->put($response->json());
-    return redirect('/authuser');
-});
-
-Route::get('/authuser', function(Request $request) {
     $access_token = $request->session()->get('access_token');
 
     $response = Http::withHeaders([
         'Accept' => 'application/json',
-        'Authorization' => 'Bearer '. $access_token
+        'Authorization' => 'Bearer ' . $access_token
     ])->get('https://accounts.feb-unsiq.ac.id/api/user');
 
     return $response->json();
 });
 
-Route::get('/logout', function(Request $request) {
-    $request->session()->forget('access_token');
-    $request->session()->forget('refresh_token');
-
-    return redirect('/');
+Route::get('/logout', function (Request $request) {
+    $access_token = $request->session()->get('access_token');
+    $response = Http::post('https://accounts.feb-unsiq.ac.id/logout');
+    dd($response);
 });
