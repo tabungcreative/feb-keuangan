10aa880164e57c9727ea2cecee00f0ddb77360d3
ahmdriffai <ahmmd.riffai@gmail.com>
2022-10-08 15:10:57 +0700

    menambahkan fitur migrasi aset

---
+++ database/migrations/2022_10_08_143642_create_aset_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aset', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_aktiva');
+            $table->string('kode_aktiva');
+            $table->string('harga_perolehan');
+            $table->string('masa_tahan');
+            $table->date('tanggal_beli');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aset');
+    }
+}
